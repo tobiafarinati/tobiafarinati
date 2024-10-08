cdd33a93427aa9bc8f47491f2033446d83a971dc
tobiafarinati <tobiafarinati@gmail.com>
2024-10-08 10:46:22 +0200

    ver13

--- script2.js
+++ script2.js
@@ -9,54 +9,46 @@ var btn = document.getElementById("myBtn");
 var span = document.getElementsByClassName("close")[0];
 
 // When the user clicks the button, open the modal 
-btn.onclick = function() {
+btn.onclick = function () {
   modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+span.onclick = function () {
   modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
+window.onclick = function (event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
-  
+
 //image-hover
-var topPosition = ($(window).height() - $(this).find('img').height()) /2;
+var topPosition = ($(window).height() - $(this).find('img').height()) / 2;
 
 $('.item').mousemove(function (e) {
-    $(this).find('img').addClass('visible');
-    $(this).find('img').css({
-      right: '8px',
-      top: (topPosition + ($(this).find('img').height() /2)) + 'px' 
-    });
+  $(this).find('img').addClass('visible');
+  $(this).find('img').css({
+    right: '8px',
+    top: (topPosition + ($(this).find('img').height() / 2)) + 'px'
+  });
 }).mouseleave(function () {
-    $(this).find('img').removeClass('visible');
+  $(this).find('img').removeClass('visible');
 });
 
-//image-rotation
-//$(document).ready(function () {
-  // Seleziona tutte le immagini all'interno degli elementi `.item`
- // $('.item > img').each(function () {
-      // Genera una rotazione casuale tra -10 e 10 gradi
-   //   var randomRotation = Math.floor(Math.random() * 21) - 10;
-      // Applica la rotazione all'immagine
-   ////       transform: 'rotate(' + randomRotation + 'deg)'
- //     });
- // });
-//});
+// BACKGROUND IMAGES
+const images = [
+  'imgbackground/2.jpg',
+  'imgbackground/3.jpg',
+  'imgbackground/4.jpg',
+  'imgbackground/6.jpg',
+  'imgbackground/8.jpg'
+];
 
+// Select a random image
+const randomImage = images[Math.floor(Math.random() * images.length)];
 
-//SafeArea Iphone
-const setVh = () => {
-  let vh = window.innerHeight * 0.01;
-  document.documentElement.style.setProperty('--vh', `${vh}px`);
-};
-
-window.addEventListener('resize', setVh);
-window.addEventListener('orientationchange', setVh);
-setVh();
+// Set the background image for the .background div
+document.querySelector('.background').style.backgroundImage = `url(${randomImage})`;
